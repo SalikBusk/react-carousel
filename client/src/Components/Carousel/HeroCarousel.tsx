a9c22dacddd0b4aaa8ac1dd307f65e778fe0bcb1
salikbubu <salikbula@gmail.com>
2023-08-03 02:36:12 +0200

    hero carousel

--- client/src/Components/Carousel/HeroCarousel.tsx
+++ client/src/Components/Carousel/HeroCarousel.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-
-import {BiSolidChevronRight, BiSolidChevronLeft} from 'react-icons/bi'
+import { BiSolidChevronRight, BiSolidChevronLeft } from 'react-icons/bi';
 
 interface Props {
   imgs: string[];
@@ -21,27 +20,31 @@ const HeroCarousel: React.FC<Props> = ({ imgs }) => {
     );
   };
 
+  if (!imgs || imgs.length === 0) {
+    console.error("No images provided.");
+    return null;
+  }
+
   return (
-    <div className='w-full rounded-[10px] relative flex flex-cols'>
-        <div className='w-full aspect-16/9 transform opacity-0 duration-75 ease-in-out'>
-            {imgs.map((img, index) => (
-                <img key={index} src={img} alt="" className={currentIndex === index ? "" : "hidden"}/>
-            ))}
-        </div>
-        <div className='carousel-control'>
-            <button className='absolute left-0 top-1/2 transform translate-y-[-80%] border-0 p-4 flex justify-center items-center cursor-pointer' onClick={handlePrevious}>
-                <BiSolidChevronLeft size={20}/>
-            </button>
-            <button className='absolute right-0 top-1/2 transform translate-y-[-80%] border-0 p-4 flex justify-center items-center cursor-pointer' onClick={handleNext}>
-                <BiSolidChevronRight size={20}/>
-            </button>
-        </div>
-        <div className='flex items-center gap-[10px] m-[16px]'>
-            {imgs.map((_, index) => (
-                <div key={index} className={`w-[15px] h-[15px] bg-black rounded-full ${currentIndex === index ? "active" : ""}`}></div>
-            ))}
-        </div>
-    </div>
+    <figure className='w-full h-[80vh] rounded-[10px] relative overflow-hidden'>
+      {imgs.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt=""
+            className={currentIndex === index ? "block w-full h-full object-cover" : "hidden"}
+            onError={() => console.error(`Error loading image at index ${index}: ${img}`)}
+          />
+        ))}
+      <figcaption className='w-full'>
+        <button className='absolute left-0 top-1/2 transform translate-y-[-80%] border-0 p-4 flex justify-center items-center cursor-pointer' onClick={handlePrevious}>
+          <BiSolidChevronLeft size={20} />
+        </button>
+        <button className='absolute right-0 top-1/2 transform translate-y-[-80%] border-0 p-4 flex justify-center items-center cursor-pointer' onClick={handleNext}>
+          <BiSolidChevronRight size={20} />
+        </button>
+      </figcaption>
+    </figure>
   );
 };
 
--- client/src/Pages/Hero.tsx
+++ client/src/Pages/Hero.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import HeroCarousel from "../Components/Carousel/HeroCarousel";
+import Container from "../Components/Container";
+
+import bg1 from "../Assets/images/Arctic-ice-tours-web.jpg";
+import bg2 from "../Assets/images/Baggrund.webp";
+
+const imageUrls = [bg1, bg2];
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="w-full h-screen">
+      <div className="py-[100px]">
+        <Container>
+          <HeroCarousel imgs={imageUrls} />
+        </Container>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
